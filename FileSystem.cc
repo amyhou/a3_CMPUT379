9cc9b64eb392780608b794302aab1b41c8083f91
Amy Hou <amyhou98@gmail.com>
2019-11-21 16:02:25 -0700

    added source code file

---
+++ FileSystem.cc
@@ -0,0 +1,60 @@
+#include "FileSystem.h"
+
+/* GLOBAL VARIABLES */
+uint8_t buffer[1024];
+
+/* FUNCTION DEFINITIONS */
+void fs_mount(char *new_disk_name)
+{
+
+}
+
+void fs_create(char name[5], int size)
+{
+
+}
+
+void fs_delete(char name[5])
+{
+
+}
+
+void fs_read(char name[5], int block_num)
+{
+
+}
+
+void fs_write(char name[5], int block_num)
+{
+
+}
+
+void fs_buff(uint8_t buff[1024])
+{
+
+}
+
+void fs_ls(void)
+{
+
+}
+
+void fs_resize(char name[5], int new_size)
+{
+
+}
+
+void fs_defrag(void)
+{
+
+}
+
+void fs_cd(char name[5])
+{
+
+}
+
+int main()
+{
+  return 0;
+}
